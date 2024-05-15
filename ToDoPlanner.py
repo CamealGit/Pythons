e296dc63110046e7d034e7ebbd543fee5be57f55
Cameal <Meilo@gmail.com>
2024-05-15 21:14:39 +0200

    finished

--- ToDoPlanner.py
+++ ToDoPlanner.py
@@ -1,11 +1,10 @@
 import tkinter as tk
+from customtkinter import *
 
+app = CTk()
+app.geometry("500x400")
+set_appearance_mode("dark")
 
-root = tk.Tk()
-root.geometry("400x400")
-
-
-print("Witaj w aplikacji do zarządzania swoją listą zadań")
-
-root.mainloop()
-
+btn1 = CTkButton(master=app, text="Hello World", fg_color="#4158D0", corner_radius=32, hover_color="#C850C0,", border_color="#FFCC70")
+btn1.place(relx=0.5, rely=0.5, anchor=CENTER)
+app.mainloop()
